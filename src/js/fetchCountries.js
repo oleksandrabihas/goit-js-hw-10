251186879bbbfed021a4c34192786df048a2406f
Oleksandra <sashka.bigas@gmail.com>
2023-05-25 10:16:09 +0200

    Update fetchCountries.js

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -2,13 +2,13 @@ const BASE_URL = 'https://restcountries.com/v3.1';
 const params = new URLSearchParams({
   fields: 'name,capital,population,flags,languages,',
 });
- function fetchCountries(name) {
-  return fetch(`${BASE_URL}/name/${name}?${params}`)
-    .then(resp => {
-    if (!resp.ok) {
+
+ async function fetchCountries(name) {
+  const resp = await fetch(`${BASE_URL}/name/${name}?${params}`)
+  if (!resp.ok) {
       throw new Error(resp.status);
     }
       return resp.json();
-  });
- }
+  }
+ 
 export { fetchCountries };
